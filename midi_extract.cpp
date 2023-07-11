f09c5c59cb525a2d95dc7c5f36fc1ef15381b2cc
설인혜 <tjfdlsgp123@naver.com>
2023-07-11 11:07:13 +0900

    Update midi_extract.cpp

--- midi_extract.cpp
+++ midi_extract.cpp
@@ -1,5 +1,3 @@
-// SS+MIDI 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -18,7 +16,7 @@ short * Read_File(int * length)
 	short * Source;
 	FILE* fr;
 
-	if((fr = fopen("C:\\Users\\inhye\\Desktop\\sssnr5.snd", "rb")) == NULL){
+	if((fr = fopen("C:\\Users\\inhye\\Desktop\\snr5.snd", "rb")) == NULL){
 		printf("file not found\n");
 		return 0;
 	}
@@ -61,85 +59,25 @@ int main()
 	short * Source;  
 	double * Hamming_window;
 	double * win_sig; 
-	double * nwin_sig;
 	
 	double * Y_real_value;
 	double * Y_image_value;
-	double * Y_phase;
 	double * Y_power;
-	
-	double * D_real_value;
-	double * D_image_value;
-	double * D_power;
-	
-	double * Noise; //for noise
-
-	double * frame1;
-	double * frame2;
-	double * frame3;
-	double * frame4;
-	double * frame5;
-	
-	double * X; // noisy - noise
 	
 	Source = Read_File(&length);
 	Hamming_window = Genarate_HW();
 	
-	int midi_num = length/Frame_Size; //midi 개수  
+	int midi_num = length/Frame_Size; //midi number
 	double midi[160];
 	
 	win_sig = (double *)malloc(sizeof(double)*Frame_Size);
-	nwin_sig = (double *)malloc(sizeof(double)*Frame_Size);
-	X = (double *)malloc(sizeof(double)*Frame_Size);
 	
 	Y_real_value = (double *)malloc(sizeof(double)*Frame_Size);
 	Y_image_value = (double *)malloc(sizeof(double)*Frame_Size);
-	Y_phase = (double *)malloc(sizeof(double)*Frame_Size);
 	Y_power = (double *)malloc(sizeof(double)*Frame_Size);	
 	
-	D_real_value = (double *)malloc(sizeof(double)*Frame_Size); 
-	D_image_value = (double *)malloc(sizeof(double)*Frame_Size); 
-	D_power = (double *)malloc(sizeof(double)*Frame_Size);
-	Noise = (double *)malloc(sizeof(double)*(Frame_Size));
-
-	//noise = 초기 5 frame//
-	frame1 = (double *)malloc(sizeof(double)*(Frame_Size));
-	frame2 = (double *)malloc(sizeof(double)*(Frame_Size));
-	frame3 = (double *)malloc(sizeof(double)*(Frame_Size));
-	frame4 = (double *)malloc(sizeof(double)*(Frame_Size));
-	frame5 = (double *)malloc(sizeof(double)*(Frame_Size));
- 
-	//noise extract
-	for(i=0;i<Frame_Size;i++){
-		frame1[i]=Source[i];
-		frame2[i]=Source[i+Frame_Size];
-		frame3[i]=Source[i+Frame_Size*2];
-		frame4[i]=Source[i+Frame_Size*3];
-		frame5[i]=Source[i+Frame_Size*4];
-	}
-	//Noise = 5 frames avg
-	for (i=0;i<Frame_Size;i++){
-		Noise[i]=(frame1[i]+frame2[i]+frame3[i]+frame4[i]+frame5[i])/5;  
-	}
-		
-	//noise DFT	
-	for(j=0;j<DFT_point;j++){
-		D_real_value[j] = D_image_value[j] = 0;
-	}
-		
-	for(j=0;j<DFT_point;j++)
-	{
-		for(k=0;k<DFT_point;k++)
-		{
-			D_real_value[j] += Noise[k]*cos(2*pi*j*k/DFT_point);
-			D_image_value[j]-= Noise[k]*sin(2*pi*j*k/DFT_point);
-		}
-	D_power[j] =sqrt(D_real_value[j]*D_real_value[j] + D_image_value[j]*D_image_value[j]); // abs(D)
-	}			
-	
-	
 	//Frame by frame
-	for(i=40000;i<length;i+=Frame_Size)
+	for(i=0;i<length;i+=Frame_Size)
 	{
 		// windowing //
 		for(j=0;j<Frame_Size;j++){
@@ -163,18 +101,8 @@ int main()
 				Y_image_value[j]-= win_sig[k]*sin(2*pi*j*k/DFT_point);
 			}
 			// abs(Source)
-			Y_power[j] =sqrt(Y_real_value[j]*Y_real_value[j] + Y_image_value[j]*Y_image_value[j]);	
-			//Y_phase[j] =atan2(Y_image_value[j],Y_real_value[j]);	
+			Y_power[j] =sqrt(Y_real_value[j]*Y_real_value[j] + Y_image_value[j]*Y_image_value[j]);		
 		}		
-		
-		//초기화 	
-		for(j=0;j<Frame_Size;j++){
-			X[j]=0;
-		}
-	
-		for(j=0;j<Frame_Size;j++){
-			X[j]=fabs(Y_power[j]-D_power[j]);
-		}
 		
 		//peak picking
 		max = 0;
@@ -183,8 +111,8 @@ int main()
 		
 		for(k=0;k<Frame_Size/2;k++)
 		{ 
-			if(X[k] > max){
-				max = X[k];
+			if(Y_power[k] > max){
+				max = Y_power[k];
 				index = k;			
 			}
 		}
@@ -193,17 +121,18 @@ int main()
 		
 		midinote = round(69+12*log2(f0/440));
 		
-		int num = (i-40000)/Frame_Size;  //Frame number
+		int num = i/Frame_Size;  //Frame number
 	    midi[num] = midinote;
+	    
 	}
 	
 	//error check
-	int num2 = (length-40000)/Frame_Size;  
+	int num2 = length/Frame_Size;  
 	for(i=1;i<(num2)-1;i++)
 	{
 		double bdiff = fabs(midi[i]-midi[i-1]);
 		double ndiff = fabs(midi[i+1]-midi[i]);
-		if((bdiff>5) && (ndiff>5)){    //미와 도 사이가 4지만 두 음의 각 오차값 1정도를 생각해서 6 
+		if((bdiff>5) && (ndiff>5)){    
 			midi[i] = 0;
 			if(midi[i-1]==midi[i+1]){
 				midi[i] = midi[i-1];
@@ -215,17 +144,17 @@ int main()
 		else if((bdiff<=5) && (ndiff>5)){
 			midi[i] = midi[i-1];
 		}
-	
 	}
-	
+
 	//humming pedding
 	for(i=num2;i<160;i++){
 		midi[i] = 0;   	//묵음 
 	}
+	
 	for(i=0;i<160;i++){
 		printf("%lf\n",midi[i]);
 	}
-	
+
 	//matching_rate
 	matching_rate = match(midi);
 	printf("MATCHING RATE : %lf",matching_rate);
@@ -307,12 +236,12 @@ double match(double *midi)
 	diff = 0;
 	for(i=0;i<75;i++){
 		diff = fabs(midi[i]-song[i]);
-		if(diff>2){   
+		if(diff>2){   //음의 차이가 3부터는 다른 음  
 			error++;
 		}
 		diff = 0;
 	}
-	for(i=75;i<80;i++){                     //묵음 부분 따로 채점  
+	for(i=75;i<80;i++){                    
 		if((midi[i]>39) && (midi[i]<49)){   
 			error++;
 		}
@@ -320,7 +249,7 @@ double match(double *midi)
 	}
 	for(i=80;i<160;i++){
 		diff = fabs(midi[i]-song[i]);
-		if(diff>2){  
+		if(diff>2){    
 			error++;
 		}
 		diff = 0;
